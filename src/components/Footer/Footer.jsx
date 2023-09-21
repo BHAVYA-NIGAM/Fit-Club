6fac013b97a71963f8453b824161828f48777340
Bhavya Nigam <bhavyanigam@Bhavyas-MacBook-Air.local>
2023-09-21 18:47:31 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+import Github from './../../assets/github.png';
+import Instagram from './../../assets/instagram.png';
+import LinkedIn from './../../assets/linkedin.png';
+import Logo from './../../assets/logo.png';
+
+export const Footer = () => {
+  return (
+    <div className="Footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="Github" />
+          <img src={Instagram} alt="Instagram" />
+          <img src={LinkedIn} alt="LinkdeIn" />
+        </div>
+        <div className="logo-f">
+          <img src={Logo} alt="" />
+        </div>
+      </div>
+      <div className="blur blur-footer-1"></div>
+      <div className="blur blur-footer-2"></div>
+    </div>
+  );
+};
